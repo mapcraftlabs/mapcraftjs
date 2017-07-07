c8b8dc258497f5b1fdbde10d6b741436cb1d75ed
fscottfoti <fscottfoti@gmail.com>
2017-07-07 14:58:32 -0700

    protest against undefined in hash

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import ss from 'simple-statistics';
 
 const hashStr = (str, mod) => {
   let hash = 0, i, chr;
-  if (str.length === 0) return hash;
+  if (str === undefined || str.length === 0) return hash;
   for (i = 0; i < str.length; i++) {
     chr   = str.charCodeAt(i);
     hash  = ((hash << 5) - hash) + chr;
